dcf6ffc534dec9c9e82d2291170130f2dd667edc
boundy99 <abdoulayeboundydjikine@gmail.com>
2022-12-07 22:06:04 -0500

    formatting

--- my-app/src/components/Header.jsx
+++ my-app/src/components/Header.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Heading() {
   return (
-    <header>Keeper</header>
+    <header><h1>Keeper</h1></header>
   )
 }
--- my-app/src/components/Note.jsx
+++ my-app/src/components/Note.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Note() {
   return (
-    <div>
+    <div className='note'>
     <h1>Title</h1>
     <p>Content</p>
     </div>
